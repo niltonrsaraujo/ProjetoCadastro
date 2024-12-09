7d3acbed9ec1d14133b33a71c233a3c47913c9ab
Nilton Araújo <139482028+niltonrsaraujo@users.noreply.github.com>
2024-12-09 19:15:41 -0300

    Update wsgi.py

--- projeto_cad_usuarios/wsgi.py
+++ projeto_cad_usuarios/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for projeto_cad_usuarios project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Adicione o caminho do diretório do projeto
+path = '/home/niltinho/ProjetoCadastro'  # Atualize com o caminho correto do seu projeto
+if path not in sys.path:
+    sys.path.append(path)
 
+# Defina o módulo de configurações
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_cad_usuarios.settings')
 
+# Ative a aplicação WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
