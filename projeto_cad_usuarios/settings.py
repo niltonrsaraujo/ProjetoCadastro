2d42e939781e338e915387456afc09fb10369b8e
Nilton Araújo <139482028+niltonrsaraujo@users.noreply.github.com>
2024-12-09 19:10:35 -0300

    Update settings.py

--- projeto_cad_usuarios/settings.py
+++ projeto_cad_usuarios/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b=&^#$owc$m7!^(gspn=)n-@7v=k$u@nogv8tv1+ea5t1&u0g6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['niltinho.pythonanywhere.com']
 
